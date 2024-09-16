8c41b98e5456f38e1eb9302de2c84339c39598c6
y <dnusrnsaenprasiththi@gmail.com>
2024-09-16 19:57:39 +0700

    UD

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-  import Home from "./routes/Home";
-  import { Route, Router } from "@solidjs/  ";
-  import MainLayout from "./components/MainLayout";
+import Home from "./routes/Home";
+import { Route, Router } from "@solidjs/router";
+import MainLayout from "./components/MainLayout";
 
-  function App() {
-    return (
-      <>
-        <MainLayout>
-          <Router root={MainLayout}>
-            <Route path="/" component={Home} />
-          </Router>
-        </MainLayout>
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <MainLayout>
+        <Router root={MainLayout}>
+          <Route path="/" component={Home} />
+        </Router>
+      </MainLayout>
+    </>
+  );
+}
 
-  export default App;
+export default App;
